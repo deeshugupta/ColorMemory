5e1cb39f9345624c713ae014eb0aa660bbc750f6
Deepanshu Gupta <gupta.deeshu@gmail.com>
2015-05-01 21:14:37 +0530

    Progress bar changes

--- ColorMemory/gen/com/colormem/ui/R.java
+++ ColorMemory/gen/com/colormem/ui/R.java
@@ -10,8 +10,11 @@ package com.colormem.ui;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int apptheme_color=0x7f040000;
+    }
     public static final class dimen {
-        public static final int aboveLayout=0x7f040005;
+        public static final int aboveLayout=0x7f050005;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Example customization of dimensions originally defined in res/values/dimens.xml
@@ -19,79 +22,88 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int circle=0x7f040002;
-        public static final int circleMargin=0x7f040004;
-        public static final int maincircle=0x7f040003;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int circle=0x7f050002;
+        public static final int circleMargin=0x7f050004;
+        public static final int maincircle=0x7f050003;
     }
     public static final class drawable {
         public static final int apptheme_btn_radio_off_holo_light=0x7f020000;
         public static final int apptheme_btn_radio_on_disabled_holo_light=0x7f020001;
-        public static final int circle=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int launch_button_selector=0x7f020004;
-        public static final int radio_button_selector=0x7f020005;
-        public static final int roundrect=0x7f020006;
-        public static final int roundrect_selected=0x7f020007;
+        public static final int apptheme_scrubber_control_disabled_holo=0x7f020002;
+        public static final int apptheme_scrubber_control_focused_holo=0x7f020003;
+        public static final int apptheme_scrubber_control_normal_holo=0x7f020004;
+        public static final int apptheme_scrubber_control_pressed_holo=0x7f020005;
+        public static final int apptheme_scrubber_control_selector_holo_light=0x7f020006;
+        public static final int apptheme_scrubber_primary_holo=0x7f020007;
+        public static final int apptheme_scrubber_progress_horizontal_holo_light=0x7f020008;
+        public static final int apptheme_scrubber_secondary_holo=0x7f020009;
+        public static final int apptheme_scrubber_track_holo_light=0x7f02000a;
+        public static final int circle=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int launch_button_selector=0x7f02000d;
+        public static final int radio_button_selector=0x7f02000e;
+        public static final int roundrect=0x7f02000f;
+        public static final int roundrect_selected=0x7f020010;
     }
     public static final class id {
-        public static final int Cancel=0x7f08002e;
-        public static final int Circle1=0x7f080012;
-        public static final int Circle2=0x7f080013;
-        public static final int Circle3=0x7f080014;
-        public static final int Circle4=0x7f080015;
-        public static final int Circle5=0x7f080016;
-        public static final int Continue=0x7f080008;
-        public static final int Correct=0x7f080005;
-        public static final int Fault=0x7f08000b;
-        public static final int Level=0x7f08000d;
-        public static final int Ok=0x7f08002d;
-        public static final int Ok_volume=0x7f080036;
-        public static final int Result=0x7f080007;
-        public static final int Wrong=0x7f080006;
-        public static final int action_settings=0x7f080037;
-        public static final int animatorCircle=0x7f080019;
-        public static final int cancel=0x7f08002a;
-        public static final int changeUserName=0x7f08002c;
-        public static final int changeusername=0x7f08002b;
-        public static final int cheat=0x7f08000f;
-        public static final int delete=0x7f080029;
-        public static final int deleteuser=0x7f080028;
-        public static final int dialog_continue=0x7f08001a;
-        public static final int dialog_heading=0x7f080017;
-        public static final int dialog_repeat=0x7f08001b;
-        public static final int game=0x7f08000e;
-        public static final int gameStats=0x7f080002;
-        public static final int gamesettings=0x7f080001;
-        public static final int gamestarter=0x7f080000;
-        public static final int heading=0x7f080003;
-        public static final int item_cheats=0x7f080032;
-        public static final int item_level=0x7f080031;
-        public static final int item_username=0x7f080030;
-        public static final int linearLayout1=0x7f080004;
-        public static final int mainRow=0x7f08001d;
-        public static final int main_cheats=0x7f080020;
-        public static final int main_level=0x7f08001f;
-        public static final int main_user=0x7f080010;
-        public static final int main_username=0x7f08001e;
-        public static final int mute=0x7f080035;
-        public static final int newusername=0x7f080026;
-        public static final int okbuttonuserdialog=0x7f080027;
-        public static final int radioCheck1=0x7f080021;
-        public static final int radioCheck2=0x7f080025;
-        public static final int radioCheck3=0x7f080023;
-        public static final int result_user=0x7f080009;
-        public static final int sequenceNumber=0x7f080018;
-        public static final int statsTable=0x7f08001c;
-        public static final int tableCircle=0x7f080011;
-        public static final int tableRow_item=0x7f08002f;
-        public static final int timer=0x7f08000c;
-        public static final int timerView=0x7f08000a;
-        public static final int userList=0x7f080024;
-        public static final int username=0x7f080022;
-        public static final int volumecontrol=0x7f080033;
-        public static final int volumeseekbar=0x7f080034;
+        public static final int Cancel=0x7f09002e;
+        public static final int Circle1=0x7f090012;
+        public static final int Circle2=0x7f090013;
+        public static final int Circle3=0x7f090014;
+        public static final int Circle4=0x7f090015;
+        public static final int Circle5=0x7f090016;
+        public static final int Continue=0x7f090008;
+        public static final int Correct=0x7f090005;
+        public static final int Fault=0x7f09000b;
+        public static final int Level=0x7f09000d;
+        public static final int Ok=0x7f09002d;
+        public static final int Ok_volume=0x7f090036;
+        public static final int Result=0x7f090007;
+        public static final int Wrong=0x7f090006;
+        public static final int action_settings=0x7f090037;
+        public static final int animatorCircle=0x7f090019;
+        public static final int cancel=0x7f09002a;
+        public static final int changeUserName=0x7f09002c;
+        public static final int changeusername=0x7f09002b;
+        public static final int cheat=0x7f09000f;
+        public static final int delete=0x7f090029;
+        public static final int deleteuser=0x7f090028;
+        public static final int dialog_continue=0x7f09001a;
+        public static final int dialog_heading=0x7f090017;
+        public static final int dialog_repeat=0x7f09001b;
+        public static final int game=0x7f09000e;
+        public static final int gameStats=0x7f090002;
+        public static final int gamesettings=0x7f090001;
+        public static final int gamestarter=0x7f090000;
+        public static final int heading=0x7f090003;
+        public static final int item_cheats=0x7f090032;
+        public static final int item_level=0x7f090031;
+        public static final int item_username=0x7f090030;
+        public static final int linearLayout1=0x7f090004;
+        public static final int mainRow=0x7f09001d;
+        public static final int main_cheats=0x7f090020;
+        public static final int main_level=0x7f09001f;
+        public static final int main_user=0x7f090010;
+        public static final int main_username=0x7f09001e;
+        public static final int mute=0x7f090035;
+        public static final int newusername=0x7f090026;
+        public static final int okbuttonuserdialog=0x7f090027;
+        public static final int radioCheck1=0x7f090021;
+        public static final int radioCheck2=0x7f090025;
+        public static final int radioCheck3=0x7f090023;
+        public static final int result_user=0x7f090009;
+        public static final int sequenceNumber=0x7f090018;
+        public static final int statsTable=0x7f09001c;
+        public static final int tableCircle=0x7f090011;
+        public static final int tableRow_item=0x7f09002f;
+        public static final int timer=0x7f09000c;
+        public static final int timerView=0x7f09000a;
+        public static final int userList=0x7f090024;
+        public static final int username=0x7f090022;
+        public static final int volumecontrol=0x7f090033;
+        public static final int volumeseekbar=0x7f090034;
     }
     public static final class layout {
         public static final int activity_launch=0x7f030000;
@@ -107,17 +119,17 @@ public final class R {
         public static final int volumecontrol_setting=0x7f03000a;
     }
     public static final class menu {
-        public static final int launch=0x7f070000;
-        public static final int level_result=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int launch=0x7f080000;
+        public static final int level_result=0x7f080001;
+        public static final int main=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int game_settings=0x7f050005;
-        public static final int game_start=0x7f050004;
-        public static final int title_activity_launch=0x7f050002;
-        public static final int title_activity_level_result=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int game_settings=0x7f060005;
+        public static final int game_start=0x7f060004;
+        public static final int title_activity_launch=0x7f060002;
+        public static final int title_activity_level_result=0x7f060003;
     }
     public static final class style {
         /** 
@@ -141,10 +153,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int SeekBarAppTheme=0x7f070002;
     }
 }
